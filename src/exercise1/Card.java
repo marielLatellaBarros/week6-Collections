ed64cb1c48917f85b4b55adf81cee5b8fbd5d2b6
Mariel Latella Barros <mariel.latella@gmail.com>
2018-11-05 15:31:15 +0100

    add a Card class. A card class has a color and a value (both enums)

---
+++ src/exercise1/Card.java
@@ -0,0 +1,19 @@
+package exercise1;
+
+public class Card {
+    Color color;
+    Value value;
+
+    public Card(Color color, Value value) {
+        this.color = color;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "color=" + color +
+                ", value=" + value +
+                '}';
+    }
+}
